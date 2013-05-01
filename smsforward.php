536fdc0ac4f4bf91b8c6b777289db3c66daf7771
Brian Levine <beans@beanserver.net>
2013-05-01 12:14:00 -0700

    reduce chars

--- smsforward.php
+++ smsforward.php
@@ -3,6 +3,6 @@ header('Content-Type: application/xml');
 ?>
 <Response>
   <Sms to="<?=$_REQUEST['PhoneNumber']?>">
-<?=htmlspecialchars(substr($_REQUEST['From'] . ": " . $_REQUEST['Body'], 0, 160))?>
+<?=htmlspecialchars(substr($_REQUEST['From'] . ": " . $_REQUEST['Body'], 0, 147))?>
   </Sms>
 </Response>
