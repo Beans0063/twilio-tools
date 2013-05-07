5315713d5d93071b53c83f357ee1727a09769e17
Brian Levine <beans@beanserver.net>
2013-05-07 09:39:31 -0700

    index update

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-twilio-tools index
+<html>
+<body>
+    <h3>twilio-tools</h3>
+
+    <div style="padding:3px;">
+        <a href="smsforward.php">smsforward.php</a>
+        <div style="padding:10px;">
+            Parameters:<br/>
+            <i>PhoneNumber:</i> The phone number to forward messages to<br/>
+            <i>Example</i>:<br/>
+            <code><a href="http://twilio-tools.herokuapp.com/smsforward.php?PhoneNumber=4155551234">http://twilio-tools.herokuapp.com/smsforward.php?PhoneNumber=4155551234<a/></code>
+        </div>
+    </div>
+
+    <div style="padding:3px;">
+        <a href="smskeyword.php">smskeyword.php</a>
+        <div style="padding:10px;">
+            Parameters:<br/>
+            <i>&lt;any keyword&gt;:</i> Keyword for an autoreply<br/>
+            <i>default:</i> Default message to display if no keywords matched<br/>
+            <i>Example</i>:<br/>
+            <code><a href="http://twilio-tools.herokuapp.com/smskeyword.php?car=The car is green&boat=The boat is blue&default=Vehicle not found">http://twilio-tools.herokuapp.com/smskeyword.php?car=The car is green&boat=The boat is blue&default=Vehicle not found<a/></code>
+        </div>
+    </div>
+</body>
+</html>
+    
+    
+    
+    
