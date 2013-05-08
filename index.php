89a7ebbca0b18b5a45ceda4a23ddc534e0504617
Brian Levine <beans@beanserver.net>
2013-05-08 14:33:46 -0700

    format change

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <div style="padding:10px;">
             Parameters:<br/>
             <i>PhoneNumber:</i> The phone number to forward messages to<br/>
-            <i>Example</i>:<br/>
+            Example:<br/>
             <code><a href="http://twilio-tools.herokuapp.com/smsforward.php?PhoneNumber=4155551234">http://twilio-tools.herokuapp.com/smsforward.php?PhoneNumber=4155551234<a/></code>
         </div>
     </div>
@@ -18,7 +18,7 @@
             Parameters:<br/>
             <i>&lt;any keyword&gt;:</i> Keyword for an autoreply<br/>
             <i>default:</i> Default message to display if no keywords matched<br/>
-            <i>Example</i>:<br/>
+            Example:<br/>
             <code><a href="http://twilio-tools.herokuapp.com/smskeyword.php?car=The car is green&boat=The boat is blue&default=Vehicle not found">http://twilio-tools.herokuapp.com/smskeyword.php?car=The car is green&boat=The boat is blue&default=Vehicle not found<a/></code>
         </div>
     </div>
@@ -30,7 +30,7 @@
             <i>blocked:</i> Comma separated list of area codes to reject<br/>
             <i>redirect:</i> Url to redirect to if the caller's area code isn't blocked<br/>
             <i>invert:</i> if present only allow the area codes specified<br/>
-            <i>Example</i>:<br/>
+            Example:<br/>
             <code><a href="http://twilio-tools.herokuapp.com/areacode_block.php?From=+16043829141&block=604,778,250&redirect=http://demo.twilio.com/welcome/">http://twilio-tools.herokuapp.com/areacode_block.php?block=604,778,250&redirect=http://demo.twilio.com/welcome/<a/></code>
         </div>
     </div>
