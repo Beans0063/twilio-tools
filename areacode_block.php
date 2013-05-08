d63886b1d7ccb900bfd14c43630c225eba7f248d
Brian Levine <beans@beanserver.net>
2013-05-08 14:14:18 -0700

    move attribute

--- areacode_block.php
+++ areacode_block.php
@@ -10,7 +10,7 @@
 	$blocked_arr = explode(",", $blocked);
 	header('Content-Type: application/xml');
 	if ( (!$invert && in_array($area_code, $blocked_arr)) || ($invert && !in_array($area_code, $blocked_arr)) ) {
-	    echo "<Response reason=\"busy\"><Reject/></Response>";
+	    echo "<Response><Reject reason=\"busy\"/></Response>";
 	} else {
     echo "<Response><Redirect>" . $redirect . "</Redirect></Response>";
 	}
