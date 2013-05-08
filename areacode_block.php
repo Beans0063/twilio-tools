24ac64988feb39a469a336eea9f1defafbbcf92a
Brian Levine <beans@beanserver.net>
2013-05-08 14:11:25 -0700

    invert param

--- areacode_block.php
+++ areacode_block.php
@@ -1,11 +1,15 @@
 <?php
 	$from = $_REQUEST['From'];
 	$redirect = $_REQUEST['redirect'];
+	if (isset($_REQUEST['invert']))
+		$invert=true;
+	else
+		$invert=false;
 	$area_code = substr($from, 2,3);
 	$blocked = $_REQUEST['block'];
 	$blocked_arr = explode(",", $blocked);
 	header('Content-Type: application/xml');
-	if (in_array($area_code, $blocked_arr)) {
+	if ( (!$invert && in_array($area_code, $blocked_arr)) || ($invert && !in_array($area_code, $blocked_arr)) ) {
 	    echo "<Response><Reject/></Response>";
 	} else {
     echo "<Response><Redirect>" . $redirect . "</Redirect></Response>";
