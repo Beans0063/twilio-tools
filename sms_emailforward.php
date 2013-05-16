e20dedc0dd8f05cfe9e92188c17cbb89111e096b
Brian Levine <beans@beanserver.net>
2013-05-16 13:45:23 -0700

    add sms_emailforward.php

---
+++ sms_emailforward.php
@@ -0,0 +1,21 @@
+<?php
+
+$sendgrid = new SendGrid($_ENV["SENDGRID_USERNAME"], $_ENV["SENDGRID_PASSWORD"]);
+$to = $_REQUEST['Email'];
+$subject = "SMS from " . $_REQUEST['From'] , " to " $_REQUEST['To'];
+$body = $_REQUEST['Body'];
+$mail = new SendGrid\Mail();
+$mail->
+   addTo($to)->
+   setFrom('app11445063@heroku.com')->
+   setSubject($subject)->
+   setText($body)->
+   setHtml($body);
+
+$sendgrid->
+  smtp->
+   send($mail);
+
+header('Content-Type: application/xml');
+echo "<Response/>";
+?>
